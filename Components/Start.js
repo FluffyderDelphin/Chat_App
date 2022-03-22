059bd0b809d9e9d6b9b221dfff69b7eca1dbfcf6
Alexander Urlich <ulrichalex12@yahoo.de>
2022-03-22 11:39:11 +0100

    adding ImageBackground

--- Components/Start.js
+++ Components/Start.js
@@ -1,24 +1,43 @@
 import React from 'react';
-import { View, Text, Button, StyleSheet, TextInput } from 'react-native';
+import {
+  View,
+  Text,
+  Button,
+  StyleSheet,
+  TextInput,
+  ImageBackground,
+} from 'react-native';
 import { useState } from 'react';
+
+const backgroundImage = {
+  uri: 'https://reactnative.dev/img/tiny_logo.png',
+};
 
 export default function Screen1({ navigation }) {
   const [name, setUserName] = useState('');
 
   return (
     <View style={styles.container}>
-      <Text>Home</Text>
-      <TextInput
-        placeholder="Enter Username"
-        onChangeText={(e) => setUserName(e)}
-      />
-      <Button
-        title="Go to Chat"
-        onPress={() => {
-          navigation.navigate('Chat', { name: name });
-        }}
-        value={name}
-      />
+      <ImageBackground
+        source={backgroundImage}
+        resizeMode="cover"
+        style={styles.image}
+      >
+        <View style={styles.loginCard}>
+          <Text>Home</Text>
+          <TextInput
+            placeholder="Enter Username"
+            onChangeText={(e) => setUserName(e)}
+          />
+          <Button
+            title="Go to Chat"
+            onPress={() => {
+              navigation.navigate('Chat', { name: name });
+            }}
+            value={name}
+          />
+        </View>
+      </ImageBackground>
     </View>
   );
 }
@@ -26,7 +45,12 @@ export default function Screen1({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  image: {
+    flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
+  },
+  loginCard: {
+    backgroundColor: 'red',
   },
 });
