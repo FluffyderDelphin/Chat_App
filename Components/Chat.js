f3e2227c6b98452dbc93c06a40e8f0b97ad1154d
Alexander Urlich <ulrichalex12@yahoo.de>
2022-03-22 22:29:35 +0100

    finishind up colochange for Chat Screen

--- Components/Chat.js
+++ Components/Chat.js
@@ -4,14 +4,23 @@ import { View, Text, Button, StyleSheet } from 'react-native';
 export default function Chat({ navigation, route }) {
   useEffect(() => {
     let name = route.params.name;
-    navigation.setOptions({ title: name });
+    navigation.setOptions({
+      title: name,
+      headerStyle: bgcolor,
+    });
   });
-
+  let bgcolor = route.params.bgcolor;
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <View style={[styles.container, bgcolor]}>
       <Text>Chat! </Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
--- Components/Start.js
+++ Components/Start.js
@@ -6,11 +6,13 @@ import {
   StyleSheet,
   TextInput,
   ImageBackground,
+  TouchableOpacity,
 } from 'react-native';
 import { useState } from 'react';
 
 export default function Screen1({ navigation }) {
   const [name, setUserName] = useState('');
+  const [bgcolor, setBackGroundColor] = useState('');
 
   return (
     <View style={styles.container}>
@@ -20,18 +22,46 @@ export default function Screen1({ navigation }) {
         style={styles.image}
       >
         <View style={styles.loginCard}>
-          <Text>Home</Text>
+          <Text style={styles.my1}>Login</Text>
           <TextInput
+            style={[styles.my1, styles.loginInput]}
             placeholder="Enter Username"
             onChangeText={(e) => setUserName(e)}
           />
           <Button
             title="Go to Chat"
             onPress={() => {
-              navigation.navigate('Chat', { name: name });
+              navigation.navigate('Chat', { name: name, bgcolor: bgcolor });
             }}
             value={name}
+            style={[styles.my1, styles.color3]}
           />
+          <View style={[styles.cyrcleContainer, styles.my1]}>
+            <TouchableOpacity
+              onPress={() => {
+                setBackGroundColor(styles.color1);
+              }}
+              style={[styles.cyrcle, styles.color1]}
+            ></TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => {
+                setBackGroundColor(styles.color2);
+              }}
+              style={[styles.cyrcle, styles.color2]}
+            ></TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => {
+                setBackGroundColor(styles.color3);
+              }}
+              style={[styles.cyrcle, styles.color3]}
+            ></TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => {
+                setBackGroundColor(styles.color4);
+              }}
+              style={[styles.cyrcle, styles.color4]}
+            ></TouchableOpacity>
+          </View>
         </View>
       </ImageBackground>
     </View>
@@ -43,10 +73,47 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   image: {
-    width: '100%',
-    height: '100%',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   loginCard: {
-    backgroundColor: 'red',
+    backgroundColor: 'white',
+    width: '70%',
+    padding: 15,
+    borderRadius: 13,
+  },
+  cyrcleContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+  cyrcle: {
+    backgroundColor: 'gray',
+    height: 40,
+    width: 40,
+    borderRadius: 20,
+    margin: 5,
+    borderColor: '#667292',
+    borderWidth: 4,
+  },
+  my1: {
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  loginInput: {
+    borderBottomColor: 'gray',
+    borderBottomWidth: 1,
+  },
+  color1: {
+    backgroundColor: '#cfe0e8',
+  },
+  color2: {
+    backgroundColor: '#b7d7e8',
+  },
+  color3: {
+    backgroundColor: '#87bdd8',
+  },
+  color4: {
+    backgroundColor: '#daebe8',
   },
 });
